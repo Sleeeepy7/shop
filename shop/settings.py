c0424158bf6c8b7a826387f0d4cac5ebf9afb4d1
Sleeeepy7 <vadim.1.95@mail.ru>
2021-12-01 11:11:46 +0300

    db postgre

--- shop/settings.py
+++ shop/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'admin',
+        'PASSWORD': '147258369zaq',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
