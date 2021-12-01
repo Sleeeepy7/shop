d461db9096e878fd6ab26550784da99ffb8d052a
Sleeeepy7 <vadim.1.95@mail.ru>
2021-12-01 11:35:14 +0300

    models for list

---
+++ listings/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('-name',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+    shu = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('shu',)
