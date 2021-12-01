609a46ccb756a525565a553dd121f0a23add2ee7
Sleeeepy7 <vadim.1.95@mail.ru>
2021-12-01 11:49:38 +0300

    добавил интернфейс в админке

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,17 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'slug', 'price', 'available')
+    list_filter = ('category', 'available')
+    list_editable = ('price', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+
--- shop/settings.py
+++ shop/settings.py
@@ -9,17 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 try:
     from .local_settings import *
